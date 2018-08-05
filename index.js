c78d5485536a2be06ade6d8666ef0c8f31a9d482
Adam Duncan <adam.duncan@clock.co.uk>
2018-08-05 18:48:47 +0100

    Allow control via HTTP

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var bunyan = require('bunyan')
   , logger = bunyan.createLogger({ name: 'home-state' })
   , Primus = require('primus')
   , Emitter = require('primus-emitter')
+  , http = require('http')
+  , express = require('express')
+  , bodyParser = require('body-parser')
+  , app = express()
   , config = {}
 
 try {
@@ -10,24 +14,32 @@ try {
   logger.warn('No config file provided, using defaults')
 }
 
-var server = Primus.createServer(
-      { port: config.port || 4578
-      , transformer: 'websockets'
-      , iknowhttpsisbetter: true
-      , parser: 'JSON'
-      })
+var server = http.createServer(app).listen(config.port || 4578)
+  , primus = new Primus(server, { transformer: 'websockets', parser: 'JSON' })
   , state = config.defaultState || 'Default'
 
-server.use('emitter', Emitter)
+function setState(newState) {
+  logger.info('Changing state from "' + state  + '" to "' + newState + '"')
+  state = newState
+  primus.send('state', state)
+}
+
+app.get('/', function (req, res) {
+  res.json({ state: state })
+})
+
+app.post('/', bodyParser.json(), function (req, res) {
+  if (req.body && req.body.state) {
+    setState(req.body.state)
+  }
+  res.json({ state: state })
+})
+
+primus.use('emitter', Emitter)
 
 logger.info('Initial state set to: "' + state + '"')
 
-server.on('connection', function (spark) {
+primus.on('connection', function (spark) {
   spark.send('state', state)
-
-  spark.on('changeState', function (newState) {
-    logger.info('Changing state from "' + state  + '" to "' + newState + '"')
-    state = newState
-    server.send('state', state)
-  })
+  spark.on('changeState', setState)
 })
